fcd8ec6d0483e947b3ee678f0486e77e34eb15bb
Luca Mussi <ing.luca.mussi@gmail.com>
2014-11-06 22:17:40 +0100

    corrected z orders

--- client/components/atState.js
+++ client/components/atState.js
@@ -15,6 +15,10 @@ Template.atState.helpers({
 Template.atState.events({
     "click .item": function(e, t){
         AccountsTemplates.setState(this.name);
+        if (this.name === "verifyEmail")
+            AccountsTemplates.state.form.set("result", AccountsTemplates.texts.info.emailVerified);
+        else
+            AccountsTemplates.state.form.set("result", false);
     },
     "click .ui.dropdown.button": function(e, t){
         console.log("click");
